a208f7cf9eb161bbdfab79f617b8f07aeee35cd4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-24 19:24:10 +0100

    Update home.py

--- home.py
+++ home.py
@@ -14,9 +14,9 @@ st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
 
 #---APP---
-page_1 = st.Page("page/bunkers.py", title="Bunkers & batboxes",icon="🐀" )
+# page_1 = st.Page("page/bunkers.py", title="Bunkers & batboxes",icon="🐀" )
 # page_2 = st.Page("page/exotem_planten.py", title="Invasive plants",icon="✍️" )
-# page_3 = st.Page("page/ratten_tershelling.py", title="Rats in Terschelling",icon="↩️" )
+page_3 = st.Page("page/ratten_tershelling.py", title="Rats in Terschelling",icon="🐀" )
 # page_4 = st.Page("page/smp_app.py", title="Field map",icon="📊" )
 # page_5 = st.Page("page/smp_amsterdam.py", title="SMP Amterdam 2024",icon="📊" )
 
@@ -24,7 +24,7 @@ page_1 = st.Page("page/bunkers.py", title="Bunkers & batboxes",icon="🐀" )
 st.logo(IMAGE,  link=LINK, icon_image=IMAGE)
 
 pg = st.navigation(
-{'Aplications and projects':[page_1
+{'Aplications and projects':[page_3
                              # ,page_2,page_3,page_4
                             ],
   # 'SMP';[page_5]
