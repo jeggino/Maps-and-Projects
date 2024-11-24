605ade437d301486a1a0764a8c92212d5849d829
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-24 20:18:18 +0100

    Update smp_terschelling.py

--- page/smp_terschelling.py
+++ page/smp_terschelling.py
@@ -10,28 +10,24 @@ st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
 
 #---COSTANTS---
-title = "Bunker & Batbox Mapping Project"
+title = "SMP Terschelling 2025"
 text = """
-Het Bunker & Batbox Mapping Project is een toegewijd initiatief dat zich richt op het grondig documenteren en analyseren van de verspreiding van vleermuisroostplaatsen op het eiland Terchelling tijdens de cruciale winterslaapperiode. Dit project omvat het nauwkeurig in kaart brengen van de locaties van verschillende bunkers en vleermuiskasten verspreid over het eiland. Naast het in kaart brengen voeren we uitgebreide onderzoeken uit die niet alleen de aangetroffen vleermuissoorten identificeren, maar ook belangrijke omgevingsgegevens vastleggen, zoals temperatuur- en vochtigheidsniveaus die op het moment van elke waarneming zijn waargenomen.
-
-Ons primaire doel is om de ingewikkelde relaties tussen vleermuispopulaties en hun omringende omgevingskenmerken te onthullen. Door inzicht te krijgen in deze dynamiek, hopen we belangrijke kenmerken te identificeren die bijdragen aan het voortbestaan ​​en welzijn van vleermuissoorten in het gebied.
-
-We nodigen gepassioneerde ecologen en natuurliefhebbers van harte uit om zich bij ons aan te sluiten in dit gezamenlijke onderzoeksproject. Als u geïnteresseerd bent om deel uit te maken van dit opwindende project en meer wilt weten over hoe u kunt meedoen, aarzel dan niet om contact met ons op te nemen voor meer informatie. Uw betrokkenheid kan een groot verschil maken in ons begrip en behoud van deze opmerkelijke wezens.
+FAKE FAKE FAKE
 """
-img = "images/bunkers_1.jpg"
-link = "https://bunkers-vleermuizenkasten-dashboard.streamlit.app/"
+img = "images/smp_terschelling.jpg"
+link = "html/SMP-Terschelling-Bats.html"
 
 #---APP---
 # st.title(title)
 
 
-# st.image(img)
+st.image(img)
 # st.markdown(text)
-with open("html/SMP-Terschelling-Bats.html", "rb") as file:
+with open(link, "rb") as file:
     btn = st.download_button(
         label=":rainbow[Download html file]",
         data=file,
-        file_name="html/SMP-Terschelling-Bats.html",
+        file_name=link,
         mime="html",
     )
 "---"
