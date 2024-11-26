def8c5d1636b627c4813dd4269ab6bd5727c4b5b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-26 23:46:43 +0100

    Update home_2.py

--- page/home_2.py
+++ page/home_2.py
@@ -15,7 +15,8 @@ For past projects, we have also provided a download link so you can access relat
 :rainbow[Enjoy exploring!]
 """
 
-st.markdown(
+col_1,col_2,col_3 = st.colums(3)
+col_2.markdown(
     """<a href="https://www.elskenecologie.nl/contact-elsken-ecologie-nh-terschelling/">
     <img src="data:image/png;base64,{}" width="250">
     </a>
