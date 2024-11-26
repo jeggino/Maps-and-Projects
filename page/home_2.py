5e2e746e3e6261269cc613946659b5f9826493eb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-26 12:54:43 +0100

    Create home_2.py

---
+++ page/home_2.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from css import *
+from constants import *
+
+text= """
+Welcome to our dashboard! Here, you will find a friendly overview of our current and past projects. Each project includes a visual preview, a brief description, and a convenient hyperlink that directs you to a real-time dashboard for the latest updates. For past projects, we have also provided a download link so you can access related files. Enjoy exploring!
+"""
+
+img = 
+
+#---APP---
+
+st.image(img)
+st.markdown(text)
+"---"
