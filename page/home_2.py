b1c03c94dd8a9942e3e7bd28d70c051b9b9e6a63
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-26 13:00:24 +0100

    Update home_2.py

--- page/home_2.py
+++ page/home_2.py
@@ -4,8 +4,11 @@ from constants import *
 
 text= """
 Welcome to our dashboard! Here, you will find a friendly overview of our current and past projects. 
+
 Each project includes a visual preview, a brief description, and a hyperlink that directs you to a real-time dashboard for the latest updates. 
+
 For past projects, we have also provided a download link so you can access related files. 
+
 :rainbow[Enjoy exploring!]
 """
 
